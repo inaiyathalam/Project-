13d9b2ff94e04cd8ae88b7497c21b2a22d2b5724
SIVANANTHAM-PYTHON-LEARNER <102138544+SIVANANTHAM-PYTHON-LEARNER@users.noreply.github.com>
2023-01-28 22:43:50 +0530

    Add files via upload

---
+++ res/myapp/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path 
+from . import views
+
+urlpatterns=[
+    path('',views.index,name='index'),
+    path('login', views.login, name='login'),
+    path('register',views.register,name='register'),
+    path('new',views.new,name='new'),
+    path('res',views.res,name='res'),
+    path('res2',views.res2,name='res2'),
+    path('templ', views.templ,name='templ'),
+    path('res3',views.res3,name='res3'),
+    path('res4',views.res4,name='res4'),
+    
+]

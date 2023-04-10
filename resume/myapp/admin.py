88d2017acf2dd2682c417429656683dcb542f2b4
Praveen N <113528770+napraveen@users.noreply.github.com>
2023-04-10 17:05:27 +0530

    Add files via upload

---
+++ resume/myapp/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Person
+# Register your models here.
+from .models import Details
+
+# Register your models here.
+admin.site.register(Details)
+admin.site.register(Person)
